985ff2d2f8a422cf600a9c16dc7b05f61bdd6bde
Ekaterina Kozlova <noplace4h1de0@gmail.com>
2024-03-30 00:48:09 +0300

    Add task 1 and report

--- task01/main.py
+++ task01/main.py
@@ -25,7 +25,8 @@ def parse_args():
     parser.add_argument('--batch_size', type=int, default=256,
                         help='Batch size for training (default: 256)')
     parser.add_argument('--checkpoint', type=str, help='Path to the saved teacher\'s weights')
-    parser.add_argument('--logging_step', type=int, help='Logging step')
+    parser.add_argument('--logging_step', type=int, help='Logging step', default=20)
+    parser.add_argument('--use_mse_during_distillation', type=str, help='Defines whether to use MSE during distillation', default=False)
     args = parser.parse_args()
     return args
 
@@ -39,14 +40,23 @@ if __name__ == "__main__":
     torch.cuda.manual_seed(SEED)
     torch.backends.cudnn.deterministic = True
 
-    # TODO: data
-    train_dataset = CIFAR10(...)
-    test_dataset = CIFAR10(...)
-    train_loader, test_loader = ...
+    # data loading
+    transforms_cifar = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    train_dataset = CIFAR10(root='./data', train=True, download=True, transform=transforms_cifar)
+    test_dataset = CIFAR10(root='./data', train=True, download=True, transform=transforms_cifar)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
-    if args.task == "train":
+    # running experiment
+    wandb.login()
+    wandb.init(project="distillation", config=args)
+
+    if args.task == "finetune":
         if args.model_type == "big":
             model = resnet101(weights=ResNet101_Weights.DEFAULT)
             model.fc = torch.nn.Linear(2048, 10)
@@ -56,14 +66,16 @@ if __name__ == "__main__":
             model.layer3 = torch.nn.Conv2d(512, 1024, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
         else:
             raise Exception("Invalid model type")
-        train(model, train_loader, test_loader, device, args.logging_step)
+        train(model, train_loader, test_loader, device, args.checkpoint, args.logging_step)
     elif args.task == "distill":
         teacher = resnet101(weights=ResNet101_Weights.DEFAULT)
-        teacher.load_state_dict(args.checkpoint)
         teacher.fc = torch.nn.Linear(2048, 10)
-
+        teacher.load_state_dict(torch.load(args.checkpoint))
+        
         student = resnet101(weights=None)
         student.fc = torch.nn.Linear(2048, 10)
         student.layer3 = torch.nn.Conv2d(512, 1024, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
 
-        distill(student, teacher, train_loader, test_loader, device, args.logging_step)
+        distill(student, teacher, train_loader, test_loader, device, args.use_mse_during_distillation, args.logging_step)
+
+    wandb.finish()
--- task01/training.py
+++ task01/training.py
@@ -9,17 +9,18 @@ def stop_criterion(accuracies, eps=0.01):
     for at least two epochs in a row.
     '''
     if len(accuracies) < 3: return False
-    acc1, acc2, acc3 = accuracies
+    acc1, acc2, acc3 = accuracies[0], accuracies[1], accuracies[2]
     return torch.abs(acc1 - acc2) < eps and torch.abs(acc2 - acc3) < eps
 
 def train(model,
           train_loader,
           test_loader,
           device,
+          checkpoint,
           logging_step=20):
     model = model.to(device)
     # Use the standard Adam optimizer without scheduler.
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
     criterion = torch.nn.CrossEntropyLoss()
 
     last_accuracies = []
@@ -34,7 +35,7 @@ def train(model,
             step += 1
             images, labels = images.to(device), labels.to(device)
             logits = model(images)
-            loss = criterion(logits, labels).item()
+            loss = criterion(logits, labels)
             current_loss += loss.item() / logging_step
             prediction = logits.argmax(dim=1)
             current_accuracy += (prediction == labels).sum() / logging_step / labels.shape[0]
@@ -49,12 +50,13 @@ def train(model,
             optimizer.step()
 
         epoch += 1
-        test_accuracy = evaluate(model, test_loader, step)
+        test_accuracy = evaluate(model, test_loader, device, step)
         last_accuracies.append(test_accuracy)
         if len(last_accuracies) > 3:
             last_accuracies.pop(0)
 
     print(f"Finished training after {epoch} epochs!")
+    torch.save(model.state_dict(), checkpoint)
   
 
 # evaluation
@@ -63,21 +65,129 @@ def evaluate(model,
              device,
              step):
     model.eval()
-    for images, labels in tqdm(test_loader):
-        images, labels = images.to(device), labels.to(device)
-        logits = model(images)
-        prediction = logits.argmax(dim=1)
-        accuracy += (prediction == labels).sum() / labels.shape[0]
-        wandb.log({"accuracy_test": accuracy}, step=step)
+    accuracy = 0
+    n_batches = 0
+    with torch.no_grad():
+        for images, labels in tqdm(test_loader):
+            n_batches += 1
+            images, labels = images.to(device), labels.to(device)
+            logits = model(images)
+            prediction = logits.argmax(dim=1)
+            accuracy += (prediction == labels).sum() / labels.shape[0]
+    wandb.log({"accuracy_test": accuracy / n_batches}, step=step)
+    return accuracy / n_batches
 
 
 # distillation
+teacher_layer1_outputs = []
+teacher_layer2_outputs = []
+teacher_layer4_outputs = []
+
+student_layer1_outputs = []
+student_layer2_outputs = []
+student_layer4_outputs = []
+
+def hook_fn_teacher(module, input, output):
+    teacher_layer1_outputs.append(output[0])
+    teacher_layer2_outputs.append(output[1])
+    teacher_layer4_outputs.append(output[3])
+
+def hook_fn_student(module, input, output):
+    student_layer1_outputs.append(output[0])
+    student_layer2_outputs.append(output[1])
+    student_layer4_outputs.append(output[3])
+
 def distill(student,
             teacher,
             train_loader,
             test_loader,
             device,
+            mse=False,
             logging_step=20):
-    pass
+    criterion = torch.nn.CrossEntropyLoss()
+    mse_criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(student.parameters(), lr=3e-4)
 
+    teacher, student = teacher.to(device), student.to(device)
+    teacher.eval()
+    student.train()
+
+    teacher.layer1.register_forward_hook(hook_fn_teacher)
+    teacher.layer2.register_forward_hook(hook_fn_teacher)
+    teacher.layer4.register_forward_hook(hook_fn_teacher)
+
+    student.layer1.register_forward_hook(hook_fn_student)
+    student.layer2.register_forward_hook(hook_fn_student)
+    student.layer4.register_forward_hook
+
+    last_accuracies = []
+    epoch, step = 0, 0
+    current_loss, current_accuracy = 0, 0
+
+    while not stop_criterion(last_accuracies):
+        student.train()
+        print(f"Epoch: {epoch}")
+        for images, labels in tqdm(train_loader):
+            optimizer.zero_grad()
+            step += 1
+            images, labels = images.to(device), labels.to(device)
+            logits = student(images)
+
+            with torch.no_grad():
+                teacher_logits = teacher(images)
+
+            classification_loss = criterion(logits, labels) # student, classification loss
+            # soft-label distillation
+            distillation_loss = criterion(
+                torch.nn.functional.softmax(logits, dim=1),
+                torch.nn.functional.softmax(teacher_logits, dim=1)
+            )
+            total_loss = distillation_loss + classification_loss
+
+            if mse:
+                #  MSE loss between corresponding layer1, layer2 and layer4 features of the student and the teacher
+                mse_loss = 0
+                mse_loss += mse_criterion(torch.as_tensor(student_layer1_outputs[0]), torch.as_tensor(teacher_layer1_outputs[0]))
+                mse_loss += mse_criterion(torch.as_tensor(student_layer2_outputs[0]), torch.as_tensor(teacher_layer2_outputs[0]))
+                mse_loss += mse_criterion(torch.as_tensor(student_layer4_outputs[0]), torch.as_tensor(teacher_layer4_outputs[0]))
+                total_loss += mse_loss
+
+            # saving my RTX3060 from OOM
+            teacher_layer1_outputs.clear()
+            teacher_layer2_outputs.clear()
+            teacher_layer4_outputs.clear()
+            student_layer1_outputs.clear()
+            student_layer2_outputs.clear()
+            student_layer4_outputs.clear()
+            torch.cuda.empty_cache()
+
+            current_loss += total_loss.item() / (logging_step * 3 if mse else logging_step * 2)
+            prediction = logits.argmax(dim=1)
+            current_accuracy += (prediction == labels).sum() / logging_step / labels.shape[0]
+
+            if step % logging_step == 0:
+                wandb.log({"loss_train": current_loss}, step=step)
+                wandb.log({"accuracy_train": current_accuracy}, step=step)
+                wandb.log({"epoch": epoch}, step=step)
+                wandb.log({"distillation_loss": distillation_loss.item() / logging_step}, step=step)
+                wandb.log({"clasffication_loss": classification_loss.item() / logging_step}, step=step)
+                
+                if mse:
+                    wandb.log({"mse_loss": mse_loss.item() / logging_step}, step=step)
+                current_loss, current_accuracy = 0, 0
+
+            total_loss.backward()
+            optimizer.step()
+
+        epoch += 1
+        test_accuracy = evaluate(student, test_loader, device, step)
+        last_accuracies.append(test_accuracy)
+        if len(last_accuracies) > 3:
+            last_accuracies.pop(0)
+
+    print(f"Finished training after {epoch} epochs!")
+    if mse:
+        torch.save(student.state_dict(), "/home/h1de0us/uni/model-compression/task01/checkpoints/student_distill_mse.pt")
+    else:
+        torch.save(student.state_dict(), "/home/h1de0us/uni/model-compression/task01/checkpoints/student_distill.pt")
 
